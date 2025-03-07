0f311653a9dd916b77ac741d8369ad83a97e322c
Areeba awan <156243202+areeba-awan@users.noreply.github.com>
2025-03-07 23:16:45 +0500

    Print Hellow World

---
+++ practice.py
@@ -0,0 +1,37 @@
+# First class 
+
+# Printing 1st Program Hello World of Python Language in Terminal
+
+# Create a File of app.py or by any other name but make sure it should be .py extension
+# Open Terminal and type python app.py to run the file
+
+# Streamlit :
+# Streamlit is an open-source app framework for Machine Learning and Data Science projects.
+
+
+# First class 
+
+# print ('Hello Python World')
+
+# Output: Hello World
+
+print ('Ramadan Mubarak to My Coders')
+
+# Output: Ramadan Mubarak to My Coders
+
+print ('Welcome to Python Programming')
+
+# Output: Welcome to Python Programming
+
+print ('I am a Python Developer')
+
+# Output: I am a Python Developer
+
+print ('I am a Python Developer and I am learning Python Programming')
+
+# Output: I am a Python Developer and I am learning Python Programming
+
+print ('I am a Python Developer and I am learning Python Programming and I am a Data Scientist')
+
+
+
